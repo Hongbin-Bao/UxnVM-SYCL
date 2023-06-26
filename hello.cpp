dfb2f17237224f3ff2ec7e2447b77da021f25e37
Hongbin Bao <1450130140@qq.com>
2023-06-26 02:07:34 +0100

    Uxn VM SYCL Implementation Initial Project File

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Description:test file
+ * 
+ * Created by: Hongbin Bao
+ * Created on: 2023/6/26
+ * 
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
